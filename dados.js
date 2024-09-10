34e26111d1ce7484a9e4bec01ad03ac215fd24c5
Madu Almeida <dudacsantos450@gmail.com>
2024-09-10 13:09:22 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "NOIVA CADÁVER",
+        descricao: "A Noiva Cadáver é uma animação em stop-motion que nos transporta para um universo gótico e fantasioso criado pela mente de Tim Burton.A história gira em torno de Victor, um jovem prestes a se casar, que acidentalmente pronuncia seus votos para Emily, uma noiva morta hámuito tempo. A partir desse acontecimento, Victor é levado para ovibrante mundo dos mortos, onde vivencia aventuras e descobre osignificado do amor e da vida",
+        link: "https://pt.wikipedia.org/wiki/A_Noiva_Cadáver",
+        tags: "Noiva cadáver tim burton gótico terror"
+    },
+    {
+        titulo: "O ESTRANHO MUNDO DE JACK",
+        descricao: "O Estranho Mundo de Jack é uma animação em stop-motion que narra a história deJack Skellington, o Rei do Halloween, que se encanta pelo Natal e decide sequestrar o Papai Noel para trazer o espírito natalino para Halloween Town.",
+        link: "https://pt.wikipedia.org/wiki/O_Estranho_Mundo_de_Jack",
+        tags:"Jack Estranho mundo esqueleto natal halloween"
+    },
+    {
+        titulo: "FRANKEWEENIE",
+        descricao: "Frankenweenie é um filme de animação em stop-motion em preto e branco que conta a história de um garoto que traz seu cachorro de volta à vida através da ciência.",
+        link: "https://pt.wikipedia.org/wiki/Frankenweenie_(filme)",
+        tags:"Frank terror frakwenie "
+    }
+];
